5f746dad6469afe4f7f955307b109c680c85a71c
ilhamhafidz404 <xxspanzie@gmail.com>
2024-09-28 21:15:59 +0700

    Responsive Menu Page

--- src/sections/Menu/category/card.tsx
+++ src/sections/Menu/category/card.tsx
@@ -13,28 +13,37 @@ export default function MenuCard({
   index: number;
 }) {
   return (
-    <section id={id} className={`px-20 scroll-mt-20 ${index != 2 && "mt-28"}`}>
+    <section
+      id={id}
+      className={`px-5 md:px-10 xl:px-20 scroll-mt-20 ${index != 2 && "mt-28"}`}
+    >
       <div className="text-center">
         <Subtitle text="Our Menu" />
         <Title text={title} />
       </div>
       <div
-        className={`grid grid-cols-5 ${
+        className={`grid xl:grid-cols-5 ${
           index != 2 ? "bg-[#191f25]" : "bg-[#0e1317]"
-        } gap-14 items-center mt-10 max-h-[650px]`}
+        } xl:gap-14 items-center mt-10 max-h-[700px] xl:max-h-[650px] overflow-hidden`}
       >
         {index % 2 == 0 && (
-          <div className="col-span-2">
+          <div className="xl:col-span-2">
             <img
               src={img}
               alt="img from unsplash"
-              className="h-[650px] w-full object-cover"
+              className="h-[200px] xl:h-[650px] w-full object-cover object-center"
               width={"auto"}
               height={"auto"}
             />
           </div>
         )}
-        <div className={`col-span-3 ${index != 2 ? "pl-20" : "pr-20"}`}>
+        <div
+          className={`xl:col-span-3 ${
+            index != 2
+              ? "xl:pl-20 md:px-10 md:pt-10 sm:px-5 sm:pt-5 px-3 pt-3"
+              : "xl:pr-20 md:px-10 md:pt-10 sm:px-5 sm:pt-5 px-3 pt-3"
+          }`}
+        >
           <MenuListItem
             name="Spinach Artichoke Dip"
             description="Tastes great with tortilla chips, pita bread, or crackers."
@@ -62,11 +71,11 @@ export default function MenuCard({
           />
         </div>
         {index % 2 != 0 && (
-          <div className="col-span-2">
+          <div className="xl:col-span-2">
             <img
               src={img}
               alt="img from unsplash"
-              className="h-[650px] w-full object-cover"
+              className="h-[200px] xl:h-[650px] w-full object-cover object-center"
               width={"auto"}
               height={"auto"}
             />
--- src/sections/Menu/category/item.tsx
+++ src/sections/Menu/category/item.tsx
@@ -8,18 +8,20 @@ export default function MenuListItem({
   price: string;
 }) {
   return (
-    <div className="flex gap-10 items-start justify-between mb-9">
+    <div className="flex gap-3 md:gap-10 items-start justify-between mb-6 md:mb-9">
       <div className="w-full">
-        <div className="flex justify-start items-center gap-5">
-          <h6 className="text-gray-100 text-2xl font-semibold whitespace-nowrap">
+        <div className="flex justify-start items-center gap-3 md:gap-5">
+          <h6 className="text-gray-100 text-sm sm:text-base md:text-xl xl:text-2xl font-medium md:font-semibold whitespace-nowrap">
             {name}
           </h6>
-          <span className="w-full h-[3px] bg-[#bc8b57]"></span>
+          <span className="w-full h-[2px] xl:h-[3px] bg-[#bc8b57]"></span>
         </div>
-        <p className="text-gray-400">{description}</p>
+        <p className="text-gray-400 text-[10px] sm:text-xs md:text-sm xl:text-base">
+          {description}
+        </p>
       </div>
       <div>
-        <p className="text-3xl font-bold whitespace-nowrap text-gray-200">
+        <p className="text-sm sm:text-base md:text-2xl xl:text-3xl font-medium md:font-bold whitespace-nowrap text-gray-200">
           Rp {price}
         </p>
       </div>
