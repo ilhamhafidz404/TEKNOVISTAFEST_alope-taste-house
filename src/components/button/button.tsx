034e3dc542e94fe66085e1212e344949b36c79b9
ilhamhafidz404 <xxspanzie@gmail.com>
2024-09-29 18:12:22 +0700

    Fix testimoni scroll to mandatory

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,7 +5,7 @@ export default function Button({ text, to }: { text: string; to: string }) {
   return (
     <Link
       to={to}
-      className="py-3 px-7 rounded-md bg-[#bc8b57] hover:bg-[#bc8b57]/80 text-white text-sm inline-flex items-center gap-2 justify-center"
+      className="py-3 px-7 rounded-md bg-[#bc8b57] hover:bg-[#bc8b57]/80 text-white inline-flex items-center gap-2 justify-center"
     >
       {text}
       <ChevronRight myClass="size-4" />
